2de6b8ecbe3b1e4340e3239bf9b4dcfdd436af8d
wandsu <wandsu@github.com>
2021-06-05 15:16:37 -0300

    Primeiro commit

---
+++ src/main/java/com/br/zup/vehicontrol/VehicontrolApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.vehicontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/zup/vehicontrol/VehicontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zup.vehicontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
