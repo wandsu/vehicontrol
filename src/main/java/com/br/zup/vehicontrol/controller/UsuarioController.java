0fe1a403c238fb4850a259c24c2b19bf5f4dde95
wandsu <wandsu@github.com>
2021-06-05 21:15:28 -0300

    Adicionado status code correto (created) no cadastro de clientes

--- src/main/java/com/br/zup/vehicontrol/controller/UsuarioController.java
+++ src/main/java/com/br/zup/vehicontrol/controller/UsuarioController.java
@@ -4,9 +4,11 @@ package com.br.zup.vehicontrol.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.zup.vehicontrol.model.Usuario;
@@ -20,6 +22,7 @@ public class UsuarioController {
 	private UsuarioService usuarioService;
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Usuario cadastrar(@Valid @RequestBody Usuario usuario) {
 		return usuarioService.salvar(usuario);
 	}
